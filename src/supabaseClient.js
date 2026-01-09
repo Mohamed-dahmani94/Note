deda7d832489a9bf313053c39287b97c53547c96
mac <mac@MacBook-Pro-de-mac.local>
2026-01-09 19:05:13 +0100

    Fix missing import in supabaseClient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,3 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = "https://ueeensmyaqsbruezyjwe.supabase.co"
 const supabaseAnonKey = "sb_publishable_Pwkeq6QyrIgUvLSbo8prGQ_9LF83oBu"
 
