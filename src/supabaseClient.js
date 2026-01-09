20d736a21b7209c78ed096d220aede4355d398e9
mac <mac@MacBook-Pro-de-mac.local>
2026-01-09 18:59:44 +0100

    Fix typo in Supabase Key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 const supabaseUrl = "https://ueeensmyaqsbruezyjwe.supabase.co"
-const supabaseAnonKey = "sb_publishable_Nwkeq6QyrIgUvLSbo8prGQ_9LF83oBu"
+const supabaseAnonKey = "sb_publishable_Pwkeq6QyrIgUvLSbo8prGQ_9LF83oBu"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
